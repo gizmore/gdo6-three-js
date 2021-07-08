3ac27a247d04640e490797efa8ea945bd0089d14
Christian Busch <busch@bmpgmbh.de>
2021-07-08 15:41:12 +0200

    6.10.4-r1176

---
+++ Module_ThreeJS.php
@@ -0,0 +1,22 @@
+<?php
+namespace GDO\ThreeJS;
+
+use GDO\Core\GDO_Module;
+use GDO\Javascript\Module_Javascript;
+
+/**
+ * ThreeJS bindings for gdo6.
+ * @author gizmore
+ * @since 6.10.4
+ */
+final class Module_ThreeJS extends GDO_Module
+{
+    public $module_priority = 45;
+    
+    public function onIncludeScript()
+    {
+        $min = Module_Javascript::instance()->jsMinAppend();
+        $this->addBowerJavascript("three/build/three{$min}.js");
+    }
+
+}
